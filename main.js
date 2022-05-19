f8dc60901e67ecf0d9d61a80d25dd6ba46c94670
Aakash Bhandary <aakashbhandary3@gmail.com>
2022-05-19 17:07:51 +0530

    First Commit

---
+++ main.js
@@ -0,0 +1,15 @@
+function instagram() {
+    console.log("Hello Instagram")
+}
+
+function twitter() {
+    console.log("Hello Twitter")
+}
+
+function snap() {
+    console.log("Hello Snapchat")
+}
+
+function watsaap() {
+    console.log("Hello WhatsApp")
+}
